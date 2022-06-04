9eecd2925c940815efa5fb05c6bb5a2e099ad64f
Taavi Ansper <anspertaavi@gmail.com>
2022-06-04 13:12:34 +0300

    deleted initialD stuff, added seasons thematic

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         </div>
         <div class="container_nav">
             <table>
-                <td><a class="sisu" href="https://initiald.fandom.com/wiki/Initial_D_Wiki">Initial D Fandom wiki</a></td>
+                <td><a class="sisu" href="https://et.wikipedia.org/wiki/Aastaajad">Aastajad vikipeedia</a></td>
                 <td><a class="sisu" href="https://hk.tlu.ee">HK Koduleht</a> </td>
                 <td><a class="sisu" href="https://tigu.hk.tlu.ee/~taavi.ansper/"> Harjutamise pealeht</a></td>
                 <td><a class="sisu" href="https://moodle.edu.ee/course/view.php?id=38099">Kursuse moodle leht</a></td>
--- index.php
+++ index.php
@@ -3,9 +3,12 @@ get_header();
 ?>
 
 <div class="container_page">
+    
     <div class="container_intro">
         <p class="intro_text">
-        Initial D on jaapani tänavavõidusõidu manga-sari, mille on kirjutanud ja illustreerinud Shuichi Shigeno. See ilmus Kodansha's Weekly Young Magazine'is aastatel 1995-2013. Lugu keskendub Jaapani illegaalsete tänavasõitude maailmale, kus kogu tegevus on koondunud mägedesse ja harva linnadesse või linnapiirkondadesse ning kus esineb peamiselt drifti võidusõidustiil. Professionaalne võidusõitja ja driftimise pioneer Keiichi Tsuchiya aitas toimetamisel kaasa. Lugu keskendub Gunma prefektuurile, täpsemalt mitmetele Kantō piirkonna mägedele ja neid ümbritsevatele linnadele ja asulatele. Kuigi mõned kohad, kus tegelased võistlevad, on väljamõeldud, põhinevad kõik sarjas esinevad kohad tegelikel Jaapani paikadel.
+        Aastaajad on iga-aastaselt korduvad, looduslikult erineva ilmega aasta osad.
+
+Aastaajad ilmnevad eriti selgelt parasvöötmes, vähem lähistroopikas ja arktilistel aladel. Lähisekvatoriaalses vöötmes ja mussoonkliimaga aladel võidakse aasta jaotada kaheks aastaajaks: kuivaks ja niiskeks perioodiks. Paljudes troopilistes piirkondades selgelt ilmnevaid aastaaegu ei olegi.
         </p>
     </div>
     <div class="container_content">
@@ -17,7 +20,7 @@ get_header();
                     <section>
                         <a href="<?php the_permalink(); ?>">
                             <figure class="hover_overlay">
-                                <img src="<?php the_post_thumbnail();?>">
+                            <?php echo '<img src="' . get_the_post_thumbnail_url(get_the_ID(), "full") . '">'; ?>
                                 <figcaption>
                                     <h3><?php the_title(); ?></h3>
                                     <h4>Loe lähemalt...</h4>
